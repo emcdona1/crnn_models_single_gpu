8a142b8a7311adfae8eb7d27e90d1ccbbd7f8f9b
Beth McDonald <emcdona1@neiu.edu>
2022-07-01 17:30:44 +0000

    Update all trainer_configs to object oriented

--- hyperparameter_tuner/trainer_config.py
+++ hyperparameter_tuner/trainer_config.py
@@ -1,29 +1,47 @@
 from pathlib import Path
 from tensorflow.keras import layers
+import configparser
+from abc import ABC
 
 
-class TrainerConfiguration:
+class HandwritingConfiguration(ABC):
     def __init__(self):
-        self.NUM_EPOCHS = 15
-        self.SEED = 2
-        self.EARLY_STOPPING = True
-        # Desired image dimensions
-        self.img_width = 400
-        self.img_height = 100
-        self.IMAGE_FORMAT = 'png'
-        self.PROJECT_FOLDER = ''
-        self.IMAGE_SET_NAME = 'words'
-        self.data_dir = Path(self.PROJECT_FOLDER, self.IMAGE_SET_NAME)
-        self.metadata_file_name = 'word_metadata2.csv'
-        
-        self.CHAR_LIST: str = '\' !"#&()[]*+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+        config = configparser.ConfigParser()
+        config.read(Path('..', 'setup.cfg'))
+        self.IMAGE_SET_NAME = config['test']['TEST_IMAGE_SET_NAME']
+        self.METADATA_FILE_NAME = config['test']['TEST_METADATA_FILE_NAME']
+        self.IMAGE_FORMAT = config['project']['IMAGE_FORMAT']
+        self.MAX_LABEL_LENGTH = config.getint('project', 'MAX_LABEL_LENGTH')
+        self.BATCH_SIZE =  config.getint('project', 'BATCH_SIZE')
+        self.IMG_HEIGHT =  config.getint('project', 'IMG_HEIGHT')
+        self.IMG_WIDTH =  config.getint('project', 'IMG_WIDTH')
+        self._char_list: str = '\' !"#&()[]*+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
         self.MAX_LABEL_LENGTH = 30
-        self.characters = sorted(set(list(self.CHAR_LIST)))
+
+        self._characters = sorted(set(list(self._char_list)))
         # Mapping characters to integers
         self.char_to_num = layers.experimental.preprocessing.StringLookup(
-            vocabulary=list(self.characters), mask_token=None
+            vocabulary=list(self._characters), mask_token=None
         )
         # Mapping integers back to original characters
         self.num_to_char = layers.experimental.preprocessing.StringLookup(
-            vocabulary=list(self.characters), mask_token=None, invert=True
+            vocabulary=list(self._characters), mask_token=None, invert=True
         )
+
+
+class TrainerConfiguration(HandwritingConfiguration):
+    def __init__(self):
+        super().__init__()
+
+        self.NUM_EPOCHS = 75
+        self.SEED = 2
+        self.EARLY_STOPPING = True
+        # Desired image dimensions
+        self.PROJECT_FOLDER = ''  # only use if the Dockerfile is not in the same file directory level as IMAGE_SET_FOLDER
+        self.IMAGE_SET_FOLDER = 'IAM_Words'
+        self.data_dir = Path(self.PROJECT_FOLDER, self.IMAGE_SET_FOLDER)
+        
+        
+class TestConfiguration(HandwritingConfiguration):
+    def __init__(self):
+        super().__init__()
