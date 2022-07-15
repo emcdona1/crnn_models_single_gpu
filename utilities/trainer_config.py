1df9b4c55bae7d05f7d94744ad2964556194f819
emcdona1 <emcdona1@neiu.edu>
2022-07-15 09:41:01 -0500

    Change config to instance variable

--- utilities/trainer_config.py
+++ utilities/trainer_config.py
@@ -1,4 +1,3 @@
-import os
 from abc import ABC
 import configparser
 from pathlib import Path
@@ -7,16 +6,16 @@ from tensorflow.keras import layers
 
 class HandwritingConfiguration(ABC):
     def __init__(self):
-        config = configparser.ConfigParser()
-        if not config.read('../setup.cfg'):
+        self.config = configparser.ConfigParser()
+        if not self.config.read('../setup.cfg'):
             print('WARNING: No configuration file loaded.')
-        self.IMAGE_SET_NAME = config['test']['TEST_IMAGE_SET_NAME']
-        self.METADATA_FILE_NAME = config['test']['TEST_METADATA_FILE_NAME']
-        self.IMAGE_FORMAT = config['project']['IMAGE_FORMAT']
-        self.MAX_LABEL_LENGTH = config.getint('project', 'MAX_LABEL_LENGTH')
-        self.BATCH_SIZE =  config.getint('project', 'BATCH_SIZE')
-        self.IMG_HEIGHT =  config.getint('project', 'IMG_HEIGHT')
-        self.IMG_WIDTH =  config.getint('project', 'IMG_WIDTH')
+        self.IMAGE_SET_LOCATION = Path(self.config['test']['TEST_IMAGE_SET_NAME'])
+        self.METADATA_FILE_NAME = self.config['test']['TEST_METADATA_FILE_NAME']
+        self.IMAGE_FORMAT = self.config['project']['IMAGE_FORMAT']
+        self.MAX_LABEL_LENGTH = self.config.getint('project', 'MAX_LABEL_LENGTH')
+        self.BATCH_SIZE = self.config.getint('project', 'BATCH_SIZE')
+        self.IMG_HEIGHT = self.config.getint('project', 'IMG_HEIGHT')
+        self.IMG_WIDTH = self.config.getint('project', 'IMG_WIDTH')
         self._char_list: str = '\' !"#&()[]*+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
         self.MAX_LABEL_LENGTH = 30
 
