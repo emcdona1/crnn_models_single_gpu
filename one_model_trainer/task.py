728e142e7fd4000a5c39606a65ade15e4542916c
emcdona1 <emcdona1@neiu.edu>
2022-08-02 14:07:22 -0500

    Streamline one model trainer, and now gets batch size from config.

--- one_model_trainer/task.py
+++ one_model_trainer/task.py
@@ -9,95 +9,33 @@ from pathlib import Path
 import argparse
 import tensorflow as tf
 import pandas as pd
-
 from utilities import create_model
-from utilities import TrainerConfiguration
 from utilities import TrainDataset
-
-c = TrainerConfiguration('./setup.cfg')
-tf.random.set_seed(c.seed)
-
-
-def get_args(manual_args=None):
-    """Parses args. Must include all hyperparameters you want to specify."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--batch_size',
-        required=True,
-        type=int,
-        help='batch size')
-    parser.add_argument(
-        '--kernel_size',
-        required=True,
-        type=int,
-        help='size of kernel to use in convolutional layers (nxn square)')
-    parser.add_argument(
-        '--activation',
-        required=True,
-        type=str,
-        help='activation function to use')
-    parser.add_argument(
-        '--dropout',
-        required=True,
-        type=float,
-        help='amount of dropout in dropout layer')
-    parser.add_argument(
-        '--num_units_dense1',
-        required=True,
-        type=int,
-        help='number of units in 1st dense layer')
-    parser.add_argument(
-        '--num_units_ltsm1',
-        required=True,
-        type=int,
-        help='number of units in 1st LTSM bidirectional layer')
-    parser.add_argument(
-        '--num_units_ltsm2',
-        required=True,
-        type=int,
-        help='number of units in 2nd LTSM bidirectional layer')
-    parser.add_argument(
-        '--learning_rate',
-        required=True,
-        type=float,
-        help='learning rate')
-    args = parser.parse_args() if not manual_args else parser.parse_args(manual_args)
-    return args
 
 
 def main():
-    assert len(sys.argv) >= 2, 'Please specify a config file.'
-    config_location = Path(sys.argv[1])
-    dataset = TrainDataset(config_location)
-
     ###################################################################
     # SET ALL THE HYPERPARAMETERS HERE, WHICH WERE DETERMINED IN TUNING
-    BATCH_SIZE = 128
-    KERNEL_SIZE = 4
-    ACTIVATION_FUNCTION = 'relu'
-    LEARNING_RATE = 0.001
-    NUM_UNITS_DENSE = 256
-    NUM_UNITS_LTSM2 = 1024
+    kernel_size = 4
+    activation_function = 'relu'
+    learning_rate = 0.001
+    num_units_dense = 256
+    num_units_ltsm2 = 1024
     ###################################################################
-    NAME = 'run_55'  # change the name
     # run 55 parameters
-    DROPOUT = 0.12489316869910207
-    NUM_UNITS_LTSM1 = 512
+    dropout = 0.12489316869910207
+    num_units_ltsm1 = 512
     # run 41 parameters
-    # DROPOUT = 0.1
-    # NUM_UNITS_LTSM1 = 768
+    # dropout = 0.1
+    # num_units_ltsm1 = 768
     ###################################################################
 
-    manual_args = [f'--batch_size={BATCH_SIZE}', f'--kernel_size={KERNEL_SIZE}', f'--activation={ACTIVATION_FUNCTION}',
-                   f'--dropout={DROPOUT}', f'--num_units_dense1={NUM_UNITS_DENSE}',
-                   f'--num_units_ltsm1={NUM_UNITS_LTSM1}', f'--num_units_ltsm2={NUM_UNITS_LTSM2}',
-                   f'--learning_rate={LEARNING_RATE}']
-    args = get_args(manual_args)
-
-    dataset.create_dataset(args.batch_size)
-    model = create_model(args.kernel_size, args.activation, args.num_units_dense1, args.dropout,
-                         args.num_units_ltsm1, args.num_units_ltsm2, args.learning_rate)
-    history = model.fit(dataset.train_dataset, epochs=c.num_epochs, validation_data=dataset.validation_dataset)
+    dataset = TrainDataset(config_location)
+    dataset.create_dataset(dataset.c.batch_size)
+    tf.random.set_seed(dataset.c.seed)
+    model = create_model(kernel_size, activation_function, num_units_dense, dropout,
+                         num_units_ltsm1, num_units_ltsm2, learning_rate)
+    history = model.fit(dataset.train_dataset, epochs=dataset.c.num_epochs, validation_data=dataset.validation_dataset)
 
     results_folder = Path('saved_models')
     if not os.path.exists(results_folder):
@@ -116,7 +54,7 @@ def main():
     prediction_model = tf.keras.models.Model(
         model.get_layer(name='image').input, model.get_layer(name='dense_layer').output
     )
-    prediction_model.compile(tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE))
+    prediction_model.compile(tf.keras.optimizers.Adam(learning_rate=learning_rate))
     prediction_model_name = f'{NAME}-prediction'
     save_location = Path(results_folder, f'{prediction_model_name}.h5')
     prediction_model.save(save_location)
@@ -124,4 +62,9 @@ def main():
 
     
 if __name__ == '__main__':
+    assert len(sys.argv) == 3, 'Please specify a config file & a training run name.'
+    config_location = Path(sys.argv[1]).absolute()
+    assert config_location.is_file(), f'{config_location} is not a file.'
+    NAME = sys.argv[2]
+
     main()
