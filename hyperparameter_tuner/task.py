0db13dd480e9547f8a86eba50e021a17df8cb3f0
emcdona1 <emcdona1@neiu.edu>
2022-08-11 14:47:13 -0500

    Pickle tuner object results, move post-tuning viz to notebook.

--- hyperparameter_tuner/task.py
+++ hyperparameter_tuner/task.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import traceback
 from pathlib import Path
+import pickle
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # suppress INFO alerts about TF
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '1'  # suppress INFO alerts about oneDNN
@@ -10,7 +11,6 @@ working_dir = os.path.join(os.getcwd())
 sys.path.append(working_dir)
 
 import tensorflow as tf
-from tensorboard.plugins.hparams import api as hp
 from skopt import gp_minimize
 from skopt.space import Real, Integer
 from skopt.utils import use_named_args
@@ -18,17 +18,16 @@ from utilities import Model, TrainerConfiguration
 from utilities import TrainDataset
 from utilities import gpu_selection
 from scipy.optimize import OptimizeResult
-from skopt.plots import plot_convergence
-import matplotlib.pyplot as plt
 
 
 global dataset
-dimensions = [Integer(low=32, high=256, name='batch_size'), Integer(low=3, high=5, name='kernel_size'),
-              Integer(low=128, high=512, name='num_dense_units1'), Real(low=0.1, high=0.5, name='dropout'),
+dimensions = [Integer(low=32, high=256, name='batch_size'),
+              Integer(low=3, high=5, name='kernel_size'),
+              Integer(low=128, high=512, name='num_dense_units1'),
+              Real(low=0.1, high=0.5, name='dropout'),
               Integer(low=256, high=2048, name='num_dense_lstm1'),
               Integer(low=256, high=2048, name='num_dense_lstm2'),
               Real(low=0.0005, high=0.1, prior='log-uniform', name='learning_rate')]
-session_num = 0
 log_folder_name = Path('logs/hparam_tuning_bayes')
 
 
@@ -46,30 +45,24 @@ def main():
                                                 random_state=c.seed,
                                                 n_random_starts=3
                                                 )
-    minima: list = search_result.x
-    print(f'Lowest validation loss: {search_result.fun:.4f}')
-    print(f'''Best values found:
-    - batch_size: {minima[0]}
-    - kernel_size: {minima[1]},
-    - num_dense_units1: {minima[2]},
-    - dropout: {minima[3]},
-    - num_dense_lstm1: {minima[4]},
-    - num_dense_lstm2: {minima[5]},
-    - learning_rate: {minima[6]}''')
-    plot_convergence(search_result)
-    plt.plot()
-    plt.show()
+    save_file_name = 'tuner_search_result.pkl'
+    with open(Path(log_folder_name, save_file_name), 'wb') as f:
+        pickle.dump(search_result, f)
+        print(f'Search results saved to: {log_folder_name}/{save_file_name}')
 
 
 @use_named_args(dimensions=dimensions)
 def fit_new_model(**params):
     global dataset
     dataset.update_batch_size(params['batch_size'])
-    global session_num
     model = Model(c)
-    model.create_model(params['kernel_size'], 'relu', params['num_dense_units1'], params['dropout'],
+    model.create_model(params['kernel_size'],
+                       'relu',
+                       params['num_dense_units1'],
+                       params['dropout'],
                        params['num_dense_lstm1'],
-                       params['num_dense_lstm2'], params['learning_rate'])
+                       params['num_dense_lstm2'],
+                       params['learning_rate'])
     global log_folder_name
     log_name = f"{params['batch_size']}-{params['kernel_size']}-{params['num_dense_units1']}-{params['dropout']:0.4f}-" + \
                f"{params['num_dense_lstm1']}-{params['num_dense_lstm2']}-{params['learning_rate']:.0e}"
@@ -80,13 +73,12 @@ def fit_new_model(**params):
         write_grads=False,
         write_images=False
     )
-    history = model.model.fit(dataset.train_dataset, validation_data=dataset.validation_dataset,
+    history = model.model.fit(dataset.train_dataset,
+                              validation_data=dataset.validation_dataset,
                               epochs=c.num_epochs,
                               callbacks=[tb_callback]
                               )
     tuning_metric = history.history['val_loss'][-1]
-    run_name = f'run-{session_num}'
-    session_num += 1
     return tuning_metric
 
 
@@ -96,7 +88,6 @@ if __name__ == "__main__":
     assert config_location.is_file(), f'{config_location} is not a file.'
     c = TrainerConfiguration(config_location)
     tf.random.set_seed(c.seed)
-    METRIC_VAL_LOSS = 'val_loss'
 
     try:
         gpu = gpu_selection()
